2ef4f32d41159ee0fa7b4ea64e48c99e7d201c9a
Tamerlan Iskhakov <124900543+TamerlanBroke@users.noreply.github.com>
2023-05-25 00:39:15 +0300

    Update lab11.py

--- lab11.py
+++ lab11.py
@@ -1,46 +1,25 @@
+
 def lab11n1():
-
-    class Restaurant:
-        def __init__(self, restaurant_name, cuisine_type):
-            self.restaurant_name = restaurant_name
-            self.cuisine_type = cuisine_type
-
-        def describe_restaurant(self):
-            print(f"Ресторан {self.restaurant_name} предлагает кухню {self.cuisine_type}.")
-
+    class Restauraht:
+        def __init__(self, name, cuisine):
+            self.name = name
+            self.cuisine = cuisine
+        def discribe_restaurant(self):
+            print(f"В ресторане {self.name} подают блюда {self.cuisine} кухни")
         def open_restaurant(self):
-            print(f"Ресторан {self.restaurant_name} открыт!")
-
-
-
-    newRestaurant = Restaurant("Пибимпап", "корейская")
-
-    print(newRestaurant.restaurant_name)
-    print(newRestaurant.cuisine_type)
-
-    newRestaurant.describe_restaurant()
+            print(f"{self.name} сейчас открыт!")
+    newRestaurant = Restauraht("China Town", "Китайской")
+    print(newRestaurant.name)
+    print(newRestaurant.cuisine)
+    newRestaurant.discribe_restaurant()
     newRestaurant.open_restaurant()
-
-def lab11n2():
-
-    class Restaurant:
-        def __init__(self, restaurant_name, cuisine_type):
-            self.restaurant_name = restaurant_name
-            self.cuisine_type = cuisine_type
-
-        def describe_restaurant(self):
-            print(f"Ресторан {self.restaurant_name} предлагает кухню {self.cuisine_type}.")
-
-
-
-    restaurant1 = Restaurant("Логово Дракона", "средневековая")
-    restaurant2 = Restaurant("ШОТЫ", "философская")
-    restaurant3 = Restaurant("Кухонный", "ресторанная")
-
-
-    restaurant1.describe_restaurant()
-    restaurant2.describe_restaurant()
-    restaurant3.describe_restaurant()
+    # lab11n2
+    newRestaurant1 = Restauraht("Франция", "Французской")
+    newRestaurant2 = Restauraht("Индия", "Индийской")
+    newRestaurant3 = Restauraht("Мексика", "Мексиканской")
+    newRestaurant1.discribe_restaurant()
+    newRestaurant2.discribe_restaurant()
+    newRestaurant3.discribe_restaurant()
 
 def lab11n3():
 
@@ -51,7 +30,7 @@ def lab11n3():
             self.rating = rating
 
         def describe_restaurant(self):
-            print(f"Ресторан {self.restaurant_name} предлагает кухню {self.cuisine_type}.")
+            print(f"ресторан {self.restaurant_name} предлагает кухню {self.cuisine_type}.")
             print(f"Рейтинг ресторана: {self.rating}")
 
         def update_rating(self, new_rating):
@@ -59,16 +38,9 @@ def lab11n3():
 
 
 
-    restaurant1 = Restaurant("Ресторан на углу", "европейская", 4.5)
-
-
+    restaurant1 = Restaurant("Европа", "европейскую", 4.5)
     restaurant1.describe_restaurant()
-
-
     restaurant1.update_rating(4.7)
     restaurant1.describe_restaurant()
 
-
 lab11n1()
-lab11n2()
-lab11n3()
